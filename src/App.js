3cb252f62a7c565cf7db69059fe06e07a87f8a88
MainakRepositor <sultankhilji001@gmail.com>
2024-09-15 09:15:13 +0530

    Added placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>We are Launching Soon!</h1>
+      <img src="https://cdnl.iconscout.com/lottie/premium/thumb/hard-work-animation-download-in-lottie-json-gif-static-svg-file-formats--load-overtime-man-people-animations-4164692.gif" alt="buidling-website"></img>
     </div>
   );
 }
