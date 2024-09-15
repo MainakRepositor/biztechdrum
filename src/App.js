d586f906400c7094e02a10a20f9fc7c4c13652b2
MainakRepositor <sultankhilji001@gmail.com>
2024-09-15 11:54:20 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <h1>We are Launching Soon!</h1>
       <img src="https://cdnl.iconscout.com/lottie/premium/thumb/hard-work-animation-download-in-lottie-json-gif-static-svg-file-formats--load-overtime-man-people-animations-4164692.gif" alt="buidling-website"></img>
+      <h2>Waiting is on...</h2>
     </div>
+
+    
   );
 }
 
